08bcb21524abb8fea796fe6ff288cea1a82cb20e
s11q <s11q@puffer.fish>
2021-04-22 00:17:02 +0200

    remove attributes concept from the project (useless without ports - that are a plan9 machanism)

--- plumb.go
+++ plumb.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -12,13 +11,7 @@ import (
 	"strings"
 )
 
-type Attribute struct {
-	Key   string
-	Value string
-}
-
 type PlumbMsg struct {
-	Attr   []Attribute
 	Data   string
 	Dst    string
 	Src    string
@@ -28,7 +21,6 @@ type PlumbMsg struct {
 
 const PlumbFile = "/mnt/plumb/send"
 
-var attrFlag  = flag.String("a", "", "attributes")
 var dstFlag   = flag.String("d", "", "destination")
 var plumbFlag = flag.String("p", PlumbFile, "plumb file")
 var srcFlag   = flag.String("s", "plumb", "source")
@@ -67,12 +59,6 @@ func CookPlumbMsg(msg *PlumbMsg) error {
 	(*msg).Dst = *dstFlag
 	(*msg).Src = *srcFlag
 	(*msg).Type = *typeFlag
-	if len(*attrFlag) > 0 {
-		err := ParseAttributes(*attrFlag, msg)
-		if err != nil {
-			return err
-		}
-	}
 	if len(*wdirFlag) > 0 {
 		(*msg).Wdir = *wdirFlag
 	} else {
@@ -94,26 +80,6 @@ func CookPlumbMsg(msg *PlumbMsg) error {
 		(*msg).Data = strings.Join(flag.Args(), " ")
 	} else {
 		log.Fatalln("no data to send to the plumber")
-	}
-	return nil
-}
-
-func ParseAttributes(str string, msg *PlumbMsg) error {
-	tokens := strings.Split(str, " ")
-	(*msg).Attr = make([]Attribute, len(tokens))
-	for i, buf := range tokens {
-		if len(buf) < 3 {
-			// need at least 3 byte (e.g. a=0)
-			return errors.New("unconsistent attribute: " + buf)
-		}
-		// The attribute key must be alpha, so sep should
-		// be the index of the "=" separator.
-		sep := strings.Index(buf, "=")
-		if sep < 1 || sep == len(buf) - 1 {
-			return errors.New("inconsistent attribute: " + buf)
-		}
-		(*msg).Attr[i].Key = buf[:sep]
-		(*msg).Attr[i].Value = buf[sep + 1:]
 	}
 	return nil
 }
